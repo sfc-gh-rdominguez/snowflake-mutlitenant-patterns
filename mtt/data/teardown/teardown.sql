92d756e6f7c1b24df286fdc8d80ddc99de82472c
Rob Dominguez <rob.dominguez@snowflake.com>
2026-07-22 17:22:30 -0700

    feat(mtt): add teardown
    Adds teardown to remove the volume and all objects in snowflake related
    to mtt.*

---
+++ mtt/data/teardown/teardown.sql
@@ -0,0 +1,14 @@
+USE ROLE ACCOUNTADMIN;
+
+DROP ROLE IF EXISTS mtt_admin;
+DROP DATABASE IF EXISTS mtt_db;
+DROP WAREHOUSE IF EXISTS mtt_wh;
+DROP ROLE IF EXISTS tenant_duff_admin;
+DROP ROLE IF EXISTS tenant_duff_viewer;
+DROP ROLE IF EXISTS tenant_krusty_admin;
+DROP ROLE IF EXISTS tenant_krusty_viewer;
+
+DROP USER IF EXISTS tenant_duff_svc;
+DROP USER IF EXISTS tenant_krusty_svc;
+
+DROP SECURITY INTEGRATION IF EXISTS mtt_keycloak;
